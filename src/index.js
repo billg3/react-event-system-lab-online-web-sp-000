1f35bcf0e01ee2e10a8ff19811075a5f352644e1
github username <github email address>
2020-05-01 17:16:12 -0400

    update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom';
 import Keypad from './components/Keypad';
 import EyesOnMe from './components/EyesOnMe';
 
-ReactDOM.render(
-  <div>
-    <Keypad />
-    <EyesOnMe />
-  </div>,
-  document.getElementById('global')
+ReactDOM.render( <
+    div >
+    <
+    Keypad / >
+    <
+    EyesOnMe / >
+    <
+    /div>,
+    document.getElementById('global')
 );
